893af655c7940557ce81337dbb0fa1d1a38ef8c7
Armando <pyarmando.academia@gmail.com>
2025-03-20 17:22:13 -0500

    nuevo

---
+++ build.sh
@@ -0,0 +1,15 @@
+# Instalar dependencias del sistema antes de instalar paquetes Python
+# Agregarlo en "Build Command" en Render: chmod +x build.sh && ./build.sh
+apt-get update && apt-get install -y \
+    build-essential \
+    cmake \
+    python3-dev \
+    python3-pip \
+    libboost-all-dev \
+    libopenblas-dev \
+    liblapack-dev \
+    libx11-dev \
+    libatlas-base-dev \
+    libjpeg-dev
+
+pip install -r requirements.txt
